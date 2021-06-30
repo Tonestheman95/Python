d85f209a323b176969d938211f6453a02c73a2ce
Tonestheman95 <82612601+Tonestheman95@users.noreply.github.com>
2021-06-30 16:47:01 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'Tonesjones' # set a secret key for security purposes
 
-@app.route('/')
-def users():
-    users = [
-        {'first_name' : 'Mike', 'last_name' : 'Tyson'},
-        {'first_name' : 'Gordan', 'last_name' : 'Ryan'},
-        {'first_name' : 'Antonio', 'last_name' : 'Brunetto'},
-        {'first_name' : 'KB', 'last_name' : 'Tonel'}
-    ]
+@app.route('/')          # The "@" decorator associates this route with the function immediately following
+def hello_counter():
+    if "count" not in session:
+        session['count'] = 1
+    else:
+        session['count'] += 1
 
-    return render_template('index.html',users=users)
+    return render_template("index.html")  # Return the string 'Hello World!' as a response
 
-if __name__=="__main__":
-    app.run(debug=True)
+
+@app.route("/count", methods=["POST"])
+def view_count():
+    if request.form["alter"]=="add":
+        session["count"] += 1
+
+    elif request.form["alter"]=="reset":
+        session["count"] = 0
+
+    return redirect("/")
+
+
+
+@app.route("/destroy")
+def destroy():
+    session.clear()
+    return redirect("/") 
+
+
+
+if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
+    app.run(debug=True)    # Run the app in debug mode.
